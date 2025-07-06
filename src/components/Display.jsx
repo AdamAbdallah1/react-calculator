eb6236e5ba932ebe2f18e0bf7a01dbb4e17f83ad
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-06 21:37:19 +0300

    Creating display component with it's props

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+function Display({ value }) {
+    return (
+    <div className="rounded-2xl bg-[#4B0082] text-white p-3 shadow-cyan-200 min-h-[3rem] text-right">
+      {value || 0}
+    </div>
+  );
+}
+
+export default Display
