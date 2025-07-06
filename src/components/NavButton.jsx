76c7028c29935fb3e1790bcde8a5113a496341f3
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-06 21:48:19 +0300

    Initializing nav buttons

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,12 @@
+function NavButtons({ key, value, onClick, className, children }) {
+    return (
+        <button
+      onClick={onClick}
+      className={`text-2xl w-full cursor-pointer rounded-4xl py-4 text-white hover:bg-blue-400 ${className}`}
+    >
+      {children || value}
+    </button>
+    )
+}
+
+export default NavButton
