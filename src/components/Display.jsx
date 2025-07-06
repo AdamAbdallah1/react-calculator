2653bf507deae3adc5648c8ecbcf78aaa7e3d3f0
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-06 22:08:09 +0300

    Fixing screen problem scrollable container

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,6 @@
 function Display({ value }) {
     return (
-    <div className="rounded-2xl bg-[#4B0082] text-white p-3 shadow-cyan-200 min-h-[3rem] text-right">
+    <div className="rounded-2xl bg-[#4B0082] text-white p-3 shadow-cyan-200 min-h-[3rem] text-right overflow-x-auto whitespace-nowrap">
       {value || "|"}
     </div>
   );
